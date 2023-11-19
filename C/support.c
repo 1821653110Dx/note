46ef6d26c9e7a6764a117cc9cbc09c87f3226a82
freather-ben <3440423865@qq.com>
2023-11-19 18:53:51 +0800

    C

--- C/support.c
+++
@@ -1,9 +0,0 @@
-
-#include <stdio.h>
-
-extern int count ;
-
-void writ_extern(void)	/* def function 'write_extern', no argument, no return */
-{
-	printf("count is %d",count) ;	/* print "count is {count}" {count:double} */
-}
--- C/test.c
+++ C/test.c
@@ -1,19 +1,31 @@
 /* include */
 #include <stdio.h>	/* include stdio.h */
 
-/* def main function */
-int main()	/* def main(), return.dty = int : */
+/* def main */
+int main()	/* def main */
 {
-	const int LENGTH = 10 ;	/* def LENGTH: 10, int, const */
-	const int WIDTH = 5 ;	/* def WIDTH: 5, int, const */
-	const char NEWLINE = '\n' ;	/* def NEWLINE: '\n', char, const */
-	int area ;		/* def area: $sym, int */	
+	int a = 1 ;	/* def a: 4, int, autodel */
+	short b ;	/* def b: null, short, autodel */
+	double c ;	/* def c: null, double, autodel */
+	int* ptr ;	/* def pointer ptr: int, autodel ;	pointer is used to store 'memory address' */
 
-	area = LENGTH * WIDTH ;		/* return {LENGTH * WIDTH}, save the data to 'area' */
+	printf("the size of a is %lu\n",sizeof(a)) ;		/* print "the size of a is {sizeof(a)}\n" {sizeof(a):%lu} */
+	printf("the size of b is %lu\n",sizeof(b)) ;	/* print "the size of b is {sizeof(b)}\n" {sizeof(b):%lu} */
+	printf("the size of c is %lu\n",sizeof(c)) ;	/* print "the size of c is {sizeof(c)}\n" {sizeof(c):%lu} */
+
+	ptr = &a ;		/* rewrite ptr with a.addr */
+
+	printf("the value of a is %d\n",a) ;		/* print "the value of a is {a}\n" {a:decimal} */
+	printf("the value of pointer ptr is %d\n",*ptr) ;	/* print "the value of pointer ptr is {*ptr}\n" {*ptr:decimal} */
+
+	a = 10 ;	/* rewrite a with 10 */
+	b = (a == 1) ? 20:30 ;		/* rewrite b with {if a == 1, then return 20; otherwise, return 30} */
+
+	printf("the value of b is %d\n",b) ;	/* print "the value of b is {b}\n" {b:%d} */
+		
+	b = (a == 10) ? 20:30 ;	/* rewrite b with {if a == 10, then return 20; otherwise, return 30} */
+
+	printf("the value of b is %d",b) ;	/* print "the value of b is {b}" {b:%d} */
 	
-	printf("value of area : %d", area) ;	/* print "value of area : {area}" [area = double] */
-	printf("%c",NEWLINE) ;	/* print "{NEWLINE}" [NEWLINE = char] */
-
 	return 0 ;	/* exit */
 }
-
