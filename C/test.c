fd57d63d1eb32ca29a0d89bf5fa8b0acecf85fbb
freather-ben <3440423865@qq.com>
2023-12-14 21:16:39 +0800

    new_C

--- C/test.c
+++ C/test.c
@@ -1,32 +1,33 @@
 /* import modules: stdio.h */
+#include<stdio.h>
 
 /* decalre func to call: getAverage */
+double getAverage(int arryp[], int size) ;
 
-
-// def main
+int main()	// def main
 {
-	// def balance : {1000, 2, 3, 17, 50}, int, ls[], autodel
-	// def avg : null, double, var, autodel
+	int balance[] = {1000, 2, 3, 17, 50} ;	// def balance : {1000, 2, 3, 17, 50}, int, ls[], autodel
+	double avg ;	// def avg : null, double, var, autodel
 
-	// {get average value of every item in 'balance': call getAverage( balance, 5 )}, save it to 'avg'
+	avg = getAverage( balance, 5 ) ;	// {get average value of every item in 'balance': call getAverage( balance, 5 )}, save it to 'avg'
 	
-	// print"average value : {avg}"{avg:%f}
+	printf("average value : %f", avg) ;	// print"average value : {avg}"{avg:%f}
 
-	// exit
+	return 0 ;	// exit
 }
 
-// def func getAverage : return = double, argument = [arry[], size], default_value = [null, null], type = [int, int]
+double getAverage(int arry[], int size)	// def func getAverage : return = double, argument = [arry[], size], default_value = [null, null], type = [int, int]
 {
-	// def i : null, int, var, autodel	// autodel : current quantity's scope is within the current func and will be autodel
-	// def average : null, int, var, autodel
-	// def sum : 0, int, var, autodel
+	int i ;		// def i : null, int, var, autodel	// autodel : current quantity's scope is within the current func and will be autodel
+	double average ;	// def average : null, double, var, autodel
+	double sum = 0 ;	// def sum : 0, double, var, autodel
 
-	// set {col : i} scope : [0, size)
+	for ( i = 0 ; i < size ; i++)	// set {col : i} scope of 'arry' : [0, size)
 	{
-		// save SUM(all items in [1, col]) to 'sum'
+		sum += arry[i] ;	// save SUM(all items in [1, col]) to 'sum'
 	}
 
-	// save {sum / size} to 'average'
+	average = sum/size ;	// save {sum / size} to 'average'
 
-	// return {average}
+	return average ;	// return {average}
 }
